e2c180b8f38c32ab2010a0907d0689f2420fcc44
runday <runday198@gmail.com>
2024-03-18 20:26:48 +0400

    #11

--- medium/repeatingCharacter.cpp
+++ medium/repeatingCharacter.cpp
@@ -1,12 +1,29 @@
 #include <string>
 #include <algorithm>
 #include <iostream>
-#include <queue>
 #include <vector>
 
 class Solution {
 public:
     int characterReplacement(std::string s, int k) {
+        std::vector<int> letters(26);
+        int left = 0;
+        int right = 0;
+        int result = 0;
+        int maxCount = 0;
+
+        while (right < s.size()) {
+            letters[s[right] - 'A']++;
+            maxCount = std::max(maxCount, letters[s[right] - 'A']);
+
+            if (right - left + 1 - maxCount > k) {
+                letters[s[left] - 'A']--;
+                left++;
+            }
+            result = std::max(result, right - left + 1);
+            right++;
+        }
         
+        return result;
     }
 };
---
+++ medium/strPermutation.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+class Solution {
+public:
+    bool checkInclusion(std::string s1, std::string s2) {
+    }
+};
+
+
+int main() {
+    std::string s1 = "adc";
+    std::string s2 = "dcda";
+
+    Solution sol;
+
+    sol.checkInclusion(s1, s2);
+}
